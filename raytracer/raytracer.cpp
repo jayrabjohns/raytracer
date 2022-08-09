eadf4a7e8bfdece485a05fa4dc6114fc4d157959
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2022-08-09 20:14:42 +0100

    Create empty project

---
+++ raytracer/raytracer.cpp
@@ -0,0 +1,12 @@
+﻿// raytracer.cpp : Defines the entry point for the application.
+//
+
+#include "raytracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
